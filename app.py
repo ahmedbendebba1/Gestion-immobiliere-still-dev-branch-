9b3e3981dacbac1a09a8e4d01681fa365beaf899
unknown <ahmedbendebba@hotmail.com>
2018-09-09 17:37:20 +0200

    requêtes rest

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
-from resources.Utilisateur import UtilisateurResource
 
+from resources.Utilisateur import UtilisateurResource, UtilisateurResourceParam
+from resources.BienImmobilier import BienImmobilierResource, BienImmobilierResourceParam,BienImmobilierResourceSearch
+
+#from resources.Utilis import UtilisateurResource
 
 
 api_bp = Blueprint('api', __name__)
@@ -11,3 +14,11 @@ api = Api(api_bp)
 # Route
 api.add_resource(Hello, '/Hello')
 api.add_resource(UtilisateurResource, '/utilisateurs')
+api.add_resource(UtilisateurResourceParam, '/utilisateurs/<string:id_utilisateur>')
+api.add_resource(BienImmobilierResource, '/bienimmobiliers')
+api.add_resource(BienImmobilierResourceParam, '/bienimmobiliers/<int:id>')
+api.add_resource(BienImmobilierResourceSearch, '/bienimmobiliers/findByVille/<string:ville>')
+
+
+#api.add_resource(UtilisateurResource, '/bienimmob')
+
--- resources/Utilisateur.py
+++ resources/Utilisateur.py
@@ -2,11 +2,71 @@ from flask import request
 from flask_restful import Resource
 from Model import db, Utilisateur, UtilisateurSchema
 
-
-utilisateurs_schema = UtilisateurSchema()
+utilisateur_schema= UtilisateurSchema()
+utilisateurs_schema = UtilisateurSchema(many=True)
 
 class UtilisateurResource(Resource):
     def get(self):
         utilisateurs = Utilisateur.query.all()
         utilisateurs = utilisateurs_schema.dump(utilisateurs).data
         return {'status': 'success', 'data': utilisateurs}, 200
+    
+    def post(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+               return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data, errors = utilisateur_schema.load(json_data)
+        if errors:
+            return errors, 422
+        utilisateur = Utilisateur.query.filter_by(id_utilisateur=data['id_utilisateur']).first()
+        if utilisateur:
+            return {'message': 'Utilisateur already exists'}, 400
+        utilisateur = Utilisateur(
+            id_utilisateur = json_data['id_utilisateur'],
+            nom = json_data['nom'],
+            prenom = json_data['prenom'],
+            date_de_naissance = json_data['date_de_naissance']
+            )
+        db.session.add(utilisateur)
+        db.session.commit()
+        result = utilisateur_schema.dump(utilisateur).data
+        return { "status": 'success', 'data': result }, 201    
+
+
+class UtilisateurResourceParam(Resource):
+        def get(self, id_utilisateur):
+            print (id_utilisateur)
+            utilisateur = Utilisateur.query.filter_by(id_utilisateur=id_utilisateur)
+            utilisateur = utilisateurs_schema.dump(utilisateur).data
+            return {'status': 'success', 'data': utilisateur}, 200
+
+        def put(self, id_utilisateur):
+            json_data = request.get_json(force=True)
+            if not json_data:
+                return {'message': 'No input data provided'}, 400
+            # Validate and deserialize input
+            data, errors = utilisateur_schema.load(json_data)
+            if errors:
+                return errors, 422
+            utilisateur = Utilisateur.query.filter_by(id_utilisateur=id_utilisateur).first()
+            if not utilisateur:
+                return {'message': 'Utilisateur does not exist'}, 400
+            utilisateur.nom = data['nom']
+            utilisateur.prenom = data['prenom']
+            utilisateur.date_de_naissance = data['date_de_naissance']        
+            db.session.commit()
+
+            result = utilisateur_schema.dump(utilisateur).data
+
+            return { "status": 'success', 'data': result }, 204
+
+        def delete(self, id_utilisateur):
+            utilisateur = Utilisateur.query.filter_by(id_utilisateur=id_utilisateur).delete()
+            db.session.commit()
+            result = utilisateur_schema.dump(utilisateur).data
+            return { "status": 'success', 'data': result}, 204
+
+
+
+            
